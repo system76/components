9a29a16204b732527cbf9b68f4366cfa224360c1
Blake Kostner <git@btkostner.io>
2020-04-15 12:49:15 -0600

    test: set browser text max width to 1800

--- test/browser/sys-omnibar/with-copy-global-and-local.js
+++ test/browser/sys-omnibar/with-copy-global-and-local.js
@@ -8,7 +8,7 @@ fixture`SysOmnibar With Copy Global And Local`
 const omnibar = Selector('header')
 
 test('screenshots', async (t) => {
-  const widths = [320, 640, 1024, 2048]
+  const widths = [320, 640, 1024, 1800]
 
   // https://github.com/eslint/eslint/issues/12117
   // eslint-disable-next-line no-unused-vars
