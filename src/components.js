633a85e4225ee63ff8673c1bf04a244fe003c34c
Blake Kostner <git@btkostner.io>
2020-04-16 15:24:07 -0600

    fix: bad export file name

--- src/components.js
+++ src/components.js
@@ -3,6 +3,6 @@
  * Exports a bunch of helpful System76 components
  */
 
-export { default as SysFooterbar } from './components/sys-footbar.vue'
+export { default as SysFooterbar } from './components/sys-footerbar.vue'
 export { default as SysHeaderbar } from './components/sys-headerbar.vue'
 export { default as SysOmnibar } from './components/sys-omnibar.vue'
