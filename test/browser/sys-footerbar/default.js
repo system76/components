89f5a7830275b9c4b53cc8201e05568c56fb6ab8
Blake Kostner <git@btkostner.io>
2020-04-15 20:00:27 -0600

    test: add screenshot tests for sys-footerbar

---
+++ test/browser/sys-footerbar/default.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe'
+
+import { assertScreenshot, WIDTHS } from '../../utility/browser'
+
+fixture`SysFooterbar default`
+  .page`http://localhost:9476/iframe.html?id=components-sysfooterbar--default&viewMode=story`
+
+const header = Selector('footer')
+
+test('screenshots', async (t) => {
+  // https://github.com/eslint/eslint/issues/12117
+  // eslint-disable-next-line no-unused-vars
+  for (const width of WIDTHS) {
+    await t.resizeWindow(width, 800)
+    await assertScreenshot(header, `sys-footerbar/default-${width}`)
+  }
+})
